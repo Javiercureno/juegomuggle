0018f4eea6c2e8adad0629b162c2db3d3e754c3a
Javier Cureño <37353310+Javiercureno@users.noreply.github.com>
2023-03-15 22:35:38 -0600

    Add files via upload

---
+++ juego muggle.py
@@ -0,0 +1,27 @@
+﻿from random import randint
+
+secret_number = randint (1,10)
+
+print(
+"""
++==================================+
+| ¡Bienvenido a mi juego, muggle!  |
+| Introduce un número entero       |
+| y adivina qué número he          |
+| elegido para ti.                 |
+| Entonces,                        |
+| ¿Cuál es el número secreto?      |
++==================================+
+""")
+
+salir = False 
+
+while salir == False :
+    numero = int(input("Escribe el número correcto para salir: "))
+    
+    if numero != secret_number :
+        print("¡Ja, ja! ¡Estás atrapado en mi bucle")
+    else :
+        print("Bien hecho, muggle! Eres libre ahora")
+        salir = True
+print("Fin del Juego")
